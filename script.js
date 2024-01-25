5e29d2082af8415df23ad8ef5bc8a165699e8362
ibrahim yakubu <yakubu@qliqintegrations.com>
2024-01-25 22:27:08 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const navbarCollapse = document.getElementById("navbarCollapse");
+const navbarTogglerIcon = document.getElementById("navbarTogglerIcon");
+
+navbarTogglerIcon.onclick = function () {
+    navbarCollapse.classList.toggle("active");
+    navbarTogglerIcon.classList.toggle("active");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
